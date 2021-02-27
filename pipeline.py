aba9c44cb9560672da8a1404092d137cdddf010c
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2021-02-26 18:44:35 -0700

    a shot at ci

--- pipeline.py
+++ pipeline.py
@@ -18,7 +18,17 @@ def main() -> co.Parallel:
     return node
 
 def pr() -> co.Parallel:
-    with co.Serial() as node:
+    with co.Serial(image=img) as node:
+
+
+        # code will be in /mnt/conducto, but either:
+        # - we need it in /usr/local/src/myapp instead
+        # - we need to run:
+        #   cd /mnt/conducto && pip install -e .
+        #   for each node that needs to run the app
+
+        # both are ugly, what am I missing?
+
         node["prep"] = co.Exec("echo what goes here?")
         node["test"] = main()
     return node
