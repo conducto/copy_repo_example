9de13cd05b0aa666acaba75125e7323a12dc6ae6
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2021-03-02 16:47:21 -0700

    myapp -> myfiglet

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="myapp",
+    name="myfiglet",
     version="0.1.0.dev1",
     description="unhelpful wrappers for popular utilities",
-    packages=["myapp"],
+    packages=["myfiglet"],
     python_requires=">=3.6",
     install_requires=["sh"],
     entry_points={
         "console_scripts": [
-            "mytree = myapp.mytree:main",
-            "myfiglet = myapp.myfiglet:main",
+            "mytree = myfiglet.mytree:main",
+            "myfiglet = myfiglet.myfiglet:main",
         ]
     },
 )
